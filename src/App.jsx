f7213680ec02cbf11e1abf30428da7966b731623
Carson <carsonbarnes98@gmail.com>
2023-11-09 10:08:29 -0500

    basic routing set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import './App.css'
+// import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h2>homepage</h2>
+  },
+  {
+    path: '/music',
+    element: <h2>music</h2>
+  },
+  {
+    path: '/videos',
+    element: <h2>videos</h2>
+  },
+  {
+    path: '/live',
+    element: <h2>live</h2>
+  },
+  {
+    path: '/merch',
+    element: <h2>merch</h2>
+  },
+])
 
 function App() {
-  return (
-    <>
-      <h1>Zac</h1>
-    </>
-  )
-}
+  return <RouterProvider router={router} />
+};
 
 export default App
