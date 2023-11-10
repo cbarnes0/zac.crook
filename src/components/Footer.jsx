2c0f1a2df6d4c2bd2c151e7646c879d8523dfb5a
Carson Barnes <117684288+cbarnes0@users.noreply.github.com>
2023-11-10 12:47:48 -0500

    footer mvp finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,21 @@ import { faMusic } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     return (
-        <footer>
-            <FontAwesomeIcon icon={faInstagram} style={{color: "#000000",}} />
-            <FontAwesomeIcon icon={faYoutube} style={{color: "#000000",}} />
-            <FontAwesomeIcon icon={faSpotify} style={{color: "#000000",}} />
-            <FontAwesomeIcon icon={faMusic} style={{color: "#000000",}} />
+        <footer className='absolute bottom-0 left-0 right-0 flex justify-center border-t lg:border-hidden'>
+            <div className='text-xl flex py-3'>
+                <a className='mx-2' href='https://www.instagram.com/zac.crook/'>
+                    <FontAwesomeIcon icon={faInstagram} style={{color: "#000000",}} />
+                </a>
+                <a className='mx-2' href='https://www.youtube.com/@zac.croook'>
+                    <FontAwesomeIcon icon={faYoutube} style={{color: "#000000",}} />
+                </a>
+                <a className='mx-2' href='https://open.spotify.com/artist/77a6pibaYeq6Ol2zJ2cSaC'>
+                    <FontAwesomeIcon icon={faSpotify} style={{color: "#000000",}} />
+                </a>
+                <a className='mx-2' href='https://music.apple.com/us/artist/zac-crook/1111802998'>
+                    <FontAwesomeIcon icon={faMusic} style={{color: "#000000",}} />
+                </a>
+            </div>
         </footer>
     )
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 const Landing = () => {
-    return <h1>Landing</h1>
+    return <h1 className='text-3xl font-bold underline'>Landing</h1>
 }
 
 export default Landing;
