75f6c577f8b0bf8ce077e068ad0d2c9b537e25ef
Carson Barnes <117684288+cbarnes0@users.noreply.github.com>
2023-11-10 11:49:42 -0500

    footer logos added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faSpotify, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faMusic } from '@fortawesome/free-solid-svg-icons'
+
+
 const Footer = () => {
-    return <h2>Footer</h2>
+    return (
+        <footer>
+            <FontAwesomeIcon icon={faInstagram} style={{color: "#000000",}} />
+            <FontAwesomeIcon icon={faYoutube} style={{color: "#000000",}} />
+            <FontAwesomeIcon icon={faSpotify} style={{color: "#000000",}} />
+            <FontAwesomeIcon icon={faMusic} style={{color: "#000000",}} />
+        </footer>
+    )
 }
 
 export default Footer;
