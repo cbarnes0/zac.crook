ce62fbf52998df3fedbecabfdcf704960c4dcd6e
Carson Barnes <117684288+cbarnes0@users.noreply.github.com>
2023-11-12 11:05:51 -0500

    live music page

--- src/pages/Live.jsx
+++ src/pages/Live.jsx
@@ -1,5 +1,35 @@
 const Live = () => {
-    return <h1>Live</h1>
-}
+    return (
+        <div className="flex flex-col items-center justify-center mt-8">
+            <div className="text-center mt-8">
+                <h2 className="text-xl font-bold">Atlanta, GA</h2>
+                <p>
+                    529 Flat Shoals Ave SE
+                </p>
+                <p>
+                    Thursday, November 30th
+                </p>
+                <p>
+                    8:30pm // $15
+                </p>
+            </div>
+            <div className="text-center mt-8">
+                <h2 className="text-xl font-bold">Athens, GA</h2>
+                <p>
+                    160 Winston Dr. #9
+                </p>
+                <p>
+                    Saturday, December 2nd
+                </p>
+                <p>
+                    8pm // $10
+                </p>
+            </div>
+        </div>
+    );
+};
+
+
+
 
 export default Live;
