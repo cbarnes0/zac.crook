3d86dd1d965faa66d3a6fc9d0b2de4f8f24a38ce
Carson <carsonbarnes98@gmail.com>
2023-11-14 10:58:17 -0500

    home page image added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,12 @@
+import zaccrook from '../assets/zaccrook.PNG'
+
 const Landing = () => {
-    return <h1 className='text-3xl font-bold underline'>Landing</h1>
+    return (
+        <div className='flex flex-col align-center justify-center mt-10'>
+            <img src={zaccrook} alt='zac crook' className='md:max-w-[500px] max-w-[300px] mt-20 md:mt-0 h-auto' />
+            <h1 className='flex justify-center mt-5 font-bold'>zac crook</h1>
+        </div>
+    )
 }
 
 export default Landing;
