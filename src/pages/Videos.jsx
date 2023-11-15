4fbb439cc8d4e91d24ac92957391742b4cd961c7
Carson Barnes <117684288+cbarnes0@users.noreply.github.com>
2023-11-15 10:44:38 -0500

    embed video

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,6 +1,15 @@
 const Videos = () => {
     return (
-      <div>Video</div>
+      <div className="w-full h-0 relative" style={{ paddingBottom: '56.25%' }}>
+      <iframe
+        className="absolute top-0 left-0 w-full h-full"
+        src="https://www.youtube.com/embed/LyqoCL6Cimg?si=YKVoPuVsq10Fcy-b"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
     )
 }
 
