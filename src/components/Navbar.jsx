55bbd98984202c1ff998445d234ad07d12cbe073
Carson Barnes <117684288+cbarnes0@users.noreply.github.com>
2023-11-17 13:16:33 -0500

    navbar styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,27 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <nav>
-            <NavLink to='/' className='nav-link'>
-                Home
-            </NavLink>
-            <NavLink to='/music' className='nav-link'>
-                Music
-            </NavLink>
-            <NavLink to='/videos' className='nav-link'>
-                Videos
-            </NavLink>
-            <NavLink to='/live' className='nav-link'>
-                Live
-            </NavLink>
-            <NavLink to='https://www.instagram.com/supercoolbadass.psd/' className='nav-link'>
-                Merch
-            </NavLink>
-        </nav>
-    );
+  return (
+    <nav className="flex">
+      <NavLink to='/' className='nav-link mr-2 font-bold'>
+        Home
+      </NavLink>
+      <NavLink to='/music' className='nav-link mr-2 font-bold'>
+        Music
+      </NavLink>
+      <NavLink to='/videos' className='nav-link mr-2 font-bold'>
+        Videos
+      </NavLink>
+      <NavLink to='/live' className='nav-link mr-2 font-bold'>
+        Live
+      </NavLink>
+      <a href='https://www.instagram.com/supercoolbadass.psd/' className='nav-link font-bold' target="_blank" rel="noopener noreferrer">
+        Merch
+      </a>
+    </nav>
+  );
 };
+
 
 export default Navbar;
