2b58ae58cd256716a4c648dba500cb06bb92af23
Carson Barnes <117684288+cbarnes0@users.noreply.github.com>
2023-11-17 13:11:54 -0500

    video sizing correct

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,16 +1,23 @@
+import React from 'react';
+
 const Videos = () => {
-    return (
-      <div className="w-full h-0 relative" style={{ paddingBottom: '56.25%' }}>
-      <iframe
-        className="absolute top-0 left-0 w-full h-full"
-        src="https://www.youtube.com/embed/LyqoCL6Cimg?si=YKVoPuVsq10Fcy-b"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+  return (
+    <div className="flex justify-center items-center mt-16 mx-28 w-full h-full">
+      <div className="w-full max-w-screen-lg">
+        <div className="relative" style={{ paddingTop: '56.25%' }}>
+          <iframe
+            className="absolute top-0 left-0 w-full h-full"
+            src="https://www.youtube.com/embed/LyqoCL6Cimg?si=YKVoPuVsq10Fcy-b"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
     </div>
-    )
-}
+  );
+};
+
 
 export default Videos;
