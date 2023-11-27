6fbc294f40eaf3b74fdd167434f7a1c08addc92a
Carson <carsonbarnes98@gmail.com>
2023-11-27 10:10:35 -0500

    new image, gradient added for fun

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer';
 
 const HomeLayout = () => {
     return (
-        <div className='min-h-screen flex flex-col relative'>
+        <div className='min-h-screen flex flex-col relative bg-gradient-to-b from-white from-10% via-yellow-100 via-30% to-purple-200 to-90%'>
             <div className='flex-grow flex flex-col bg-fixed bg-cover bg-center'>
                 <div className='relative min-h-screen flex flex-col items-center text-black'>
                     <Navbar />
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,10 @@
-import zaccrook from '../assets/zaccrook.png'
+import zaccrook from '../assets/crook_liq.png'
 
 const Landing = () => {
     return (
         <div className='flex flex-col align-center justify-center mt-10'>
-            <img src={zaccrook} alt='zac crook' className='md:max-w-[500px] max-w-[300px] mt-20 md:mt-0 h-auto' />
-            <h1 className='flex justify-center mt-5 font-bold'>zac crook</h1>
+            <img src={zaccrook} alt='zac crook' className='md:max-w-[600px] max-w-[375px] mt-32 md:mt-20 h-auto' />
+            {/* <h1 className='flex justify-center mt-5 font-bold'>zac crook</h1> */}
         </div>
     )
 }
