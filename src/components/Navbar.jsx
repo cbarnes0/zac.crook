20ce64c66c54904210911849ca1dd26b097b5b2d
Carson <carsonbarnes98@gmail.com>
2023-11-27 12:54:36 -0500

    nav edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,21 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="flex">
-      <NavLink to='/' className='nav-link mr-2 font-bold'>
-        Home
+    <nav className="flex mt-5">
+      <NavLink to='/' className='nav-link mr-2 font-bold text-blue-700'>
+        home
       </NavLink>
-      <NavLink to='/music' className='nav-link mr-2 font-bold'>
-        Music
+      <NavLink to='/music' className='nav-link mr-2 font-bold text-yellow-500'>
+        music
       </NavLink>
-      <NavLink to='/videos' className='nav-link mr-2 font-bold'>
-        Videos
+      <NavLink to='/videos' className='nav-link mr-2 font-bold text-green-700'>
+        videos
       </NavLink>
-      <NavLink to='/live' className='nav-link mr-2 font-bold'>
-        Live
+      <NavLink to='/live' className='nav-link mr-2 font-bold text-purple-700'>
+        live
       </NavLink>
-      <a href='https://www.instagram.com/supercoolbadass.psd/' className='nav-link font-bold' target="_blank" rel="noopener noreferrer">
-        Merch
+      <a href='https://www.instagram.com/supercoolbadass.psd/' className='nav-link font-bold text-red-500' target="_blank" rel="noopener noreferrer">
+        merch
       </a>
     </nav>
   );
